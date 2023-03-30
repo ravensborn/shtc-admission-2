b3338613375f60f728fac713b3f39441d597f515
Yad Hoshyar <yad.dev@unimartglobal.com>
2023-03-30 04:11:17 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -138,79 +138,79 @@ class DatabaseSeeder extends Seeder
             $document = public_path('seeder_data/paper.png');
             $studentPhoto = public_path('seeder_data/student.png');
 
-            $student->addMediaFromUrl($studentPhoto)
+            $student->addMedia($studentPhoto)
                 ->usingName('student-photo')
                 ->usingFilename('student-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('student-photo');
 
             if ($student->uploaded_id_type == 1) {
-                $student->addMediaFromUrl($id)
+                $student->addMedia($id)
                     ->usingName('national-id-front-side')
                     ->usingFilename('national-id-front-side.png')
                     ->preservingOriginal()
                     ->toMediaCollection('national-id-front-side');
 
-                $student->addMediaFromUrl($id)
+                $student->addMedia($id)
                     ->usingName('national-id-back-side')
                     ->usingFilename('national-id-back-side.png')
                     ->preservingOriginal()
                     ->toMediaCollection('national-id-back-side');
             } else {
-                $student->addMediaFromUrl($id)
+                $student->addMedia($id)
                     ->usingName('id-front-side-photo')
                     ->usingFilename('id-front-side-photo.png')
                     ->preservingOriginal()
                     ->toMediaCollection('id-front-side-photo');
 
-                $student->addMediaFromUrl($id)
+                $student->addMedia($id)
                     ->usingName('id-back-side-photo')
                     ->usingFilename('id-back-side-photo.png')
                     ->preservingOriginal()
                     ->toMediaCollection('id-back-side-photo');
-                $student->addMediaFromUrl($id)
+                $student->addMedia($id)
                     ->usingName('nationality-card-photo')
                     ->usingFilename('nationality-card-photo.png')
                     ->preservingOriginal()
                     ->toMediaCollection('nationality-card-photo');
             }
 
-            $student->addMediaFromUrl($id2)
+            $student->addMedia($id2)
                 ->usingName('karty-zanyari-front-side-photo')
                 ->usingFilename('karty-zanyari-front-side-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('karty-zanyari-front-side-photo');
 
-            $student->addMediaFromUrl($id2)
+            $student->addMedia($id2)
                 ->usingName('karty-zanyari-back-side-photo')
                 ->usingFilename('karty-zanyari-back-side-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('karty-zanyari-back-side-photo');
 
-            $student->addMediaFromUrl($id2)
+            $student->addMedia($id2)
                 ->usingName('residency-confirmation-photo')
                 ->usingFilename('residency-confirmation-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('residency-confirmation-photo');
 
-            $student->addMediaFromUrl($document)
+            $student->addMedia($document)
                 ->usingName('food-card-photo')
                 ->usingFilename('food-card-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('food-card-photo');
 
-            $student->addMediaFromUrl($document)
+            $student->addMedia($document)
                 ->usingName('brwanama-12-photo')
                 ->usingFilename('brwanama-12-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('brwanama-12-photo');
 
-            $student->addMediaFromUrl($document)
+            $student->addMedia($document)
                 ->usingName('kafala-photo')
                 ->usingFilename('kafala-photo.png')
                 ->preservingOriginal()
                 ->toMediaCollection('kafala-photo');
-            $student->addMediaFromUrl($document)
+            $student->addMedia($document)
                 ->usingName('daray_psula')
                 ->usingFilename('daray_psula.png')
                 ->preservingOriginal()
