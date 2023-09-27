62c510d93e89998200d35c59b8b7be9e17130658
Yad <yad.hoshyar1@gmail.com>
2023-09-27 23:28:25 +0300

    Revert "Update AppServiceProvider.php"
    This reverts commit 88ca411b7becf45190c7a87fabd287b78d7e76be.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
-        request()->server->set('HTTPS', request()->header('X-Forwarded-Proto', 'https') == 'https' ? 'on' : 'off');
         Schema::defaultStringLength(125);
     }
 }
